81d3daeeface5fd81b5656c92978019a2a68dd5b
John Parker <jp0624@gmail.com>
2023-05-23 18:20:12 -0400

    refined GetComponent

--- src/helpers/GetComponent.tsx
+++ src/helpers/GetComponent.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react"
+
 interface Category {
 	title?: string
 	folder: string
@@ -21,6 +23,55 @@ interface Params {
 	[key: string]: string
 }
 
+function findCategory(
+	categories: Category[],
+	categoryName: string
+): Category | undefined {
+	return categories.find((cat) => cat.folder === categoryName)
+}
+
+function findModule(
+	category: Category,
+	moduleName: string
+): Module | undefined {
+	return category.modules.find((mod) => mod.folder === moduleName)
+}
+
+function fetchCategoryComponent(
+	category: Category,
+	setActiveComponent: any,
+	setActiveCategory: any,
+	setActiveModule: any
+): Component {
+	const defaultComponent: Component = {
+		title: category.title || "Missing Title",
+		component: category.component || "CategoryPage",
+	}
+
+	setActiveComponent(defaultComponent)
+	console.warn("Fetched category:", defaultComponent)
+	setActiveCategory(category)
+
+	return defaultComponent
+}
+
+function fetchModuleComponent(
+	module: Module,
+	setActiveComponent: any,
+	setActiveModule: any
+): Component {
+	const component: Component = {
+		title: module.title,
+		component: module.component,
+	}
+
+	setActiveComponent(component)
+	console.warn("Fetched category:", component)
+	setActiveModule(module)
+
+	return component
+}
+
 function ComponentFetcher(
 	categories: Category[],
 	params: Params,
@@ -30,38 +81,31 @@ function ComponentFetcher(
 ): Promise<Component> {
 	return new Promise((resolve, reject) => {
 		try {
-			const defaultComponent: Component = {
-				title: "Home Page",
-				component: "HomePage",
-			}
-			const category = categories.find(
-				(cat) => cat.folder === params.categoryName
-			)
+			const category = findCategory(categories, params.categoryName)
 			if (category) {
-				const module = category.modules.find(
-					(mod) => mod.folder === params.moduleName
-				)
-				setActiveCategory(category)
+				const module = findModule(category, params.moduleName)
 
 				if (module) {
-					const component: Component = {
-						title: module.title,
-						component: module.component,
-					}
-					setActiveModule(module)
-					setActiveComponent(component)
-					console.warn("Fetched category:", component)
+					const component = fetchModuleComponent(
+						module,
+						setActiveComponent,
+						setActiveModule
+					)
 					resolve(component)
 				} else {
-					const component: Component = {
-						title: category.title || "Missing Title",
-						component: category.component || "CategoryPage",
-					}
-					setActiveComponent(component)
-					console.warn("Fetched category:", component)
+					const component = fetchCategoryComponent(
+						category,
+						setActiveComponent,
+						setActiveCategory,
+						setActiveModule
+					)
 					resolve(component)
 				}
 			} else {
+				const defaultComponent: Component = {
+					title: "Home Page",
+					component: "HomePage",
+				}
 				resolve(defaultComponent)
 			}
 		} catch (err) {
@@ -77,22 +121,32 @@ function getActiveComponent(
 	setActiveCategory: any,
 	setActiveModule: any
 ): React.ReactElement {
-	const fetchComponent = async (): Promise<void> => {
-		try {
-			const component = await ComponentFetcher(
-				categories,
-				params,
-				setActiveComponent,
-				setActiveCategory,
-				setActiveModule
-			)
-			console.log("Fetched category:", component)
-		} catch (error) {
-			console.error(error)
+	const [fetching, setFetching] = useState(true)
+
+	useEffect(() => {
+		const fetchComponent = async (): Promise<void> => {
+			try {
+				const component = await ComponentFetcher(
+					categories,
+					params,
+					setActiveComponent,
+					setActiveCategory,
+					setActiveModule
+				)
+				console.log("Fetched category:", component)
+				setFetching(false)
+			} catch (error) {
+				console.error(error)
+			}
 		}
+		fetchComponent()
+	}, [])
+
+	if (fetching) {
+		return <div>Fetching category...</div>
 	}
-	fetchComponent()
-	return <div>Fetching category...</div>
+
+	return <div>Category fetched!</div>
 }
 
 export default getActiveComponent
