19fc9f159d752221df33710649e90d0744bc3dc3
jp0624 <jp0624@gmail.com>
2024-02-26 11:34:21 -0500

    added fib value detect

--- src/_db/sections/js-tests/interview/fibonacci_sequence/default.tsx
+++ src/_db/sections/js-tests/interview/fibonacci_sequence/default.tsx
@@ -1,25 +1,34 @@
-import FileImporter from "../../../../../helpers/FileImporter"
-import AddCode from "../../../../../components/structure/AddCode/AddCode"
-import code01 from "./code_01.txt"
+import FileImporter from '../../../../../helpers/FileImporter'
+import AddCode from '../../../../../components/structure/AddCode/AddCode'
+import code01 from './code_01.txt'
+import code02 from './code_02.txt'
 
 function Default() {
 	let loadedData: any = {
 		code01: FileImporter(code01),
+		code02: FileImporter(code02),
 	}
 	return (
 		<>
 			<h4>Function Description</h4>
 
 			<p>
-				Write a function to detect is a number is within the fibonacci
-				sequence
+				Write a function to detect is a number is within the fibonacci sequence
 			</p>
 			<h3>Sample Input</h3>
 
 			<code>24</code>
 
 			{loadedData.code01 ? (
-				<AddCode data={loadedData.code01} height='500' />
+				<AddCode data={loadedData.code01} height='300' />
+			) : null}
+			<p>Write a function to get value of n in fibonacci sequence</p>
+			<h3>Sample Input</h3>
+
+			<code>24</code>
+
+			{loadedData.code02 ? (
+				<AddCode data={loadedData.code02} height='500' />
 			) : null}
 		</>
 	)
