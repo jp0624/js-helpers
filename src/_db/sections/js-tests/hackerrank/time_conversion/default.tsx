974f270fa9db57db48bcd1c61d8cb7f43fab4be3
jp0624 <jp0624@gmail.com>
2024-05-08 19:12:52 -0400

    another added

--- src/_db/sections/js-tests/hackerrank/time_conversion/default.tsx
+++ src/_db/sections/js-tests/hackerrank/time_conversion/default.tsx
@@ -1,19 +1,34 @@
-import FileImporter from "../../../../../helpers/FileImporter"
-import AddCode from "../../../../../components/structure/AddCode/AddCode"
-import code01 from "./code_01.txt"
+import FileImporter from '../../../../../helpers/FileImporter'
+import AddCode from '../../../../../components/structure/AddCode/AddCode'
+import code01 from './code_01.txt'
 
 function Default() {
 	let loadedData: any = {
 		code01: FileImporter(code01),
+	}
+	function timeConversion(s: string): string {
+		const isPM = s.includes('PM')
+		const timeParts = s.slice(0, -2).split(':')
+		let hours = parseInt(timeParts[0], 10)
+
+		if (isPM && hours < 12) {
+			hours += 12
+		} else if (!isPM && hours === 12) {
+			hours = 0
+		}
+
+		const hoursString = hours.toString().padStart(2, '0')
+		const formattedTime = `${hoursString}:${timeParts[1]}:${timeParts[2]}`
+
+		return formattedTime
 	}
 	return (
 		<>
 			<h4>Function Description</h4>
 
 			<p>
-				Complete the timeConversion function in the editor below. It
-				should return a new string representing the input time in 24
-				hour format.
+				Complete the timeConversion function in the editor below. It should
+				return a new string representing the input time in 24 hour format.
 			</p>
 
 			<p>timeConversion has the following parameter(s):</p>
@@ -27,8 +42,8 @@ function Default() {
 			<h3>Input Format</h3>
 
 			<p>
-				A single string that represents a time in -hour clock format
-				(i.e.: or ).
+				A single string that represents a time in -hour clock format (i.e.: or
+				).
 			</p>
 
 			<h3>Constraints</h3>
@@ -46,6 +61,8 @@ function Default() {
 			{loadedData.code01 ? (
 				<AddCode data={loadedData.code01} height='500' />
 			) : null}
+			<code>timeConversion(07:05:45PM)</code>
+			<code>{timeConversion('07:05:45PM')}</code>
 		</>
 	)
 }
--- src/components/structure/Content/Content.tsx
+++ src/components/structure/Content/Content.tsx
@@ -1,41 +1,42 @@
-import { useContext, useEffect } from "react"
-import { useParams } from "react-router"
-import { SiteContext } from "../../../context/SiteContext"
-import styles from "./styles.module.scss"
+import { useContext, useEffect } from 'react'
+import { useParams } from 'react-router'
+import { SiteContext } from '../../../context/SiteContext'
+import styles from './styles.module.scss'
 // Compoent List
 // Global Pages
-import HomePage from "../../../pages/home/Home"
-import CategoryPage from "../../../pages/category/Category"
-import SectionPage from "../../../pages/Section/Section"
-import SearchPage from "../../../pages/search/Search"
+import HomePage from '../../../pages/home/Home'
+import CategoryPage from '../../../pages/category/Category'
+import SectionPage from '../../../pages/Section/Section'
+import SearchPage from '../../../pages/search/Search'
 
 // functions and scope
-import BlockScope from "../../../_db/sections/fundamentals/functions/block_scope/BlockScope"
-import FunctionScope from "../../../_db/sections/fundamentals/functions/function_scope/FunctionScope"
+import BlockScope from '../../../_db/sections/fundamentals/functions/block_scope/BlockScope'
+import FunctionScope from '../../../_db/sections/fundamentals/functions/function_scope/FunctionScope'
 
 //operators
-import OPBlockScope from "../../../_db/sections/fundamentals/operators/op_block_scope/BlockScope"
-import OPFunctionScope from "../../../_db/sections/fundamentals/operators/op_function_scope/FunctionScope"
+import OPBlockScope from '../../../_db/sections/fundamentals/operators/op_block_scope/BlockScope'
+import OPFunctionScope from '../../../_db/sections/fundamentals/operators/op_function_scope/FunctionScope'
 
 // ecmascript
-import es5_2009 from "../../../_db/sections/js-documentation/ecmascript/es5_2009/es5_2009"
-import es6_es2015 from "../../../_db/sections/js-documentation/ecmascript/es6_es2015/es6_es2015"
-import es7_es2016 from "../../../_db/sections/js-documentation/ecmascript/es7_es2016/es7_es2016"
-import es8_es2017 from "../../../_db/sections/js-documentation/ecmascript/es8_es2017/es8_es2017"
-import es9_es2018 from "../../../_db/sections/js-documentation/ecmascript/es9_es2018/es9_es2018"
-import es10_es2019 from "../../../_db/sections/js-documentation/ecmascript/es10_es2019/es10_es2019"
+import es5_2009 from '../../../_db/sections/js-documentation/ecmascript/es5_2009/es5_2009'
+import es6_es2015 from '../../../_db/sections/js-documentation/ecmascript/es6_es2015/es6_es2015'
+import es7_es2016 from '../../../_db/sections/js-documentation/ecmascript/es7_es2016/es7_es2016'
+import es8_es2017 from '../../../_db/sections/js-documentation/ecmascript/es8_es2017/es8_es2017'
+import es9_es2018 from '../../../_db/sections/js-documentation/ecmascript/es9_es2018/es9_es2018'
+import es10_es2019 from '../../../_db/sections/js-documentation/ecmascript/es10_es2019/es10_es2019'
 
 // js tests
-import time_conversion from "../../../_db/sections/js-tests/hackerrank/time_conversion/default"
-import plus_minus from "../../../_db/sections/js-tests/hackerrank/plus_minus/default"
-import min_max_sum from "../../../_db/sections/js-tests/hackerrank/min_max_sum/default"
-import sparse_arrays from "../../../_db/sections/js-tests/hackerrank/sparse_arrays/default"
-import lonely_integer from "../../../_db/sections/js-tests/hackerrank/lonely_integer/default"
-import flipping_bits from "../../../_db/sections/js-tests/hackerrank/flipping_bits/default"
-import diagonal_difference from "../../../_db/sections/js-tests/hackerrank/diagonal_difference/default"
+import time_conversion from '../../../_db/sections/js-tests/hackerrank/time_conversion/default'
+import plus_minus from '../../../_db/sections/js-tests/hackerrank/plus_minus/default'
+import min_max_sum from '../../../_db/sections/js-tests/hackerrank/min_max_sum/default'
+import sparse_arrays from '../../../_db/sections/js-tests/hackerrank/sparse_arrays/default'
+import lonely_integer from '../../../_db/sections/js-tests/hackerrank/lonely_integer/default'
+import flipping_bits from '../../../_db/sections/js-tests/hackerrank/flipping_bits/default'
+import diagonal_difference from '../../../_db/sections/js-tests/hackerrank/diagonal_difference/default'
+import counting_sort_1 from '../../../_db/sections/js-tests/hackerrank/counting_sort_1/default'
 
 // interview
-import fibonacci_sequence from "../../../_db/sections/js-tests/interview/fibonacci_sequence/default"
+import fibonacci_sequence from '../../../_db/sections/js-tests/interview/fibonacci_sequence/default'
 
 const Content = () => {
 	const { setActiveComponent, activeComponent, setParams } =
@@ -48,14 +49,14 @@ const Content = () => {
 		SectionPage,
 		SearchPage,
 		//--Fundamentals Section
-		////----Functions Category
+		//----Functions Category
 		BlockScope,
 		FunctionScope,
-		////----Operators Category
+		//----Operators Category
 		OPBlockScope,
 		OPFunctionScope,
 		//--JS Documentation
-		////----ECMAScript Category
+		//----ECMAScript Category
 		es5_2009,
 		es6_es2015,
 		es7_es2016,
@@ -63,9 +64,9 @@ const Content = () => {
 		es9_es2018,
 		es10_es2019,
 		//--JS Tests
-		////----Interview
+		//----Interview
 		fibonacci_sequence,
-		////----Hacker Rank
+		//----Hacker Rank
 		time_conversion,
 		plus_minus,
 		min_max_sum,
@@ -73,10 +74,11 @@ const Content = () => {
 		lonely_integer,
 		flipping_bits,
 		diagonal_difference,
+		counting_sort_1,
 	}
 	let Component = {
-		title: activeComponent.title || "Home Page",
-		component: activeComponent.component || "HomePage",
+		title: activeComponent.title || 'Home Page',
+		component: activeComponent.component || 'HomePage',
 	}
 	let DynamicComponent = components[Component.component]
 	return (
@@ -87,7 +89,7 @@ const Content = () => {
 				<h1>
 					{!!activeComponent.title
 						? activeComponent.title
-						: "Developer Helpers Guide"}
+						: 'Developer Helpers Guide'}
 				</h1>
 				<section>{<DynamicComponent />}</section>
 			</article>
